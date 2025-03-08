40df3fb54d74eebdbfc187d5b3840f549463df2c
eunsu <songess@naver.com>
2025-03-08 21:10:24 +0900

    [feat]: POST 요청의 FORM 처리를 위한 MemberForm 구현

---
+++ src/main/java/hello/hello_spring/controller/MemberForm.java
@@ -0,0 +1,16 @@
+package hello.hello_spring.controller;
+
+public class MemberForm {
+    // createMemberForm의 input의 name 속성 값인 name과 같아야 함
+    private String name;
+
+    // getter
+    public String getName() {
+        return name;
+    }
+
+    // setter
+    public void setName(String name) {
+        this.name = name;
+    }
+}
